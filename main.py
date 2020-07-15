d4bdd312af591d5472f35cfbd0bb1909e674590b
MyronFields1 <68288662+MyronFields@users.noreply.github.com>
2020-07-15 15:36:20 +0000

    Everything mostly

---
+++ main.py
@@ -0,0 +1,10 @@
+a = "Myron Fields"
+b = "Lil Uzi Vert"
+c = "Comezel"
+d = "Gaming"
+e = "06/20/22"
+
+
+print("My name is" + a + ") print("and my favorite rapper" + b + "")
+print("I love to go on vacation to"  " + c + ") print("and my two main hobbies are") + d +")
+print("Today's date is" + e + ")
