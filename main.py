81204a91da6869564b3fcb7e760bc7cc6d8ca945
MyronFields1 <68288662+MyronFields@users.noreply.github.com>
2020-07-16 15:40:31 +0000

    Psuedo

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-annual_rainfall_2019 = annual_rainfall_2019
+#Take User input
+#Convert input to str
+userInput = str(input("give me a character to turn into a cube... "))
 
-january_rainfall_2019 = 3.15
-febuary_rainfall_2019 = 3.64
-march_rainfall_2019 = 4.14
+#diamond varible 1
+d_1= userInput+" "+userInput+" "+userInput+" "+userInput
+#diamond varible 2
+d_2 = userInput+"    "+userInput
 
-winter_rainfall_2019 = january_rainfall_2019 + febuary_rainfall_2019 + march_rainfall_2019 + avg_winter_rainfall_2019 = winter_rainfall_2019 / 3 
 
-
-print("The total winter rainfall is", winter_rainfall_2019, "inches")
-print("The average winter rainfall is", avg_winterfall_2019, "inches"
-
-annual_rainfall + winter_rainfall_2019
+#Print ,
