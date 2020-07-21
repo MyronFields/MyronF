96d7852c995deaa0d64844ff20978d8ae20c2b55
MyronFields1 <68288662+MyronFields@users.noreply.github.com>
2020-07-21 17:48:05 +0000

    part 1 missing 100000 iterations

--- main.py
+++ main.py
@@ -16,6 +16,14 @@ print("Random integer from 1 to 6")
 num2 = random.randint(1,6)
 print("random integer: ", num2)
 
+print("Random integer from 100 to 10000 ")
+num1 = random.randint(100,100000)
+print("Random integer: ", num1)
+
+print("Random integer from 1 to 6")
+num2 = random.randint(1,6)
+print("random integer: ", num2)
+
 # of iterations: 10
 # 1 = 11.11%
 # 2 = 33.33%
@@ -32,7 +40,13 @@ print("random integer: ", num2)
 # 5 = 4.71%
 # 6 = 1.23%
 
-
+# of iterations: 100000
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
 
 
 
