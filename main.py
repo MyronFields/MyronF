7ba5075774a633af04afcdf41cf34ee8b89eb51c
MyronFields1 <68288662+MyronFields@users.noreply.github.com>
2020-07-23 17:29:14 +0000

    TRINGLE

--- main.py
+++ main.py
@@ -1,53 +1,15 @@
-import random
+def triangle(n): 
+    k = 2*n - 2
+  
+    for i in range(0, n): 
+      
+        for j in range(0, k): 
+            print(end=" ") 
+      
+        k = k - 1
 
-print("Random integer from 0 to 10")
-num1 = random.randint(0,9)
-print("Random integer: ", num1)
-
-print("Random integer from 1 to 6")
-num2 = random.randint(1,6)
-print("random integer: ", num2)
-
-print("Random integer from 10 to 100")
-num1 = random.randint(10,100)
-print("Random integer: ", num1)
-
-print("Random integer from 1 to 6")
-num2 = random.randint(1,6)
-print("random integer: ", num2)
-
-print("Random integer from 100 to 10000 ")
-num1 = random.randint(100,100000)
-print("Random integer: ", num1)
-
-print("Random integer from 1 to 6")
-num2 = random.randint(1,6)
-print("random integer: ", num2)
-
-# of iterations: 10
-# 1 = 11.11%
-# 2 = 33.33%
-# 3 = 83.33%
-# 4 = 80.00%
-# 5 = 66.67%
-# 6 = 14.28%
-
-# of iterations: 100
-# 1 = 10.00%
-# 2 = 3.26%
-# 3 = 3.41%
-# 4 = 26.08%
-# 5 = 4.71%
-# 6 = 1.23%
-
-# of iterations: 100000
-# 1
-# 2
-# 3
-# 4
-# 5
-# 6
-
-
-
-
+        for j in range(0, i+1):  
+            print("* ", end="") 
+        print("\r")  
+n = 5
+triangle(n)
