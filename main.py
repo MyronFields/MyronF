200096b5c803e8e00ed5dca0b5e5b749fa8fd193
MyronFields1 <68288662+MyronFields@users.noreply.github.com>
2020-07-24 15:38:20 +0000

    4loop

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-def triangle(n): 
-    k = 2*n - 2
-  
-    for i in range(0, n): 
-      
-        for j in range(0, k): 
-            print(end=" ") 
-      
-        k = k - 1
+print("outer loop") 
+print("inner loop")
+print("inner loop")
+print("inner loop")
+print("inner loop")
+print("inner loop")
 
-        for j in range(0, i+1):  
-            print("* ", end="") 
-        print("\r")  
-n = 5
-triangle(n)
+
+for i in range(0,5):
+   print(i)
+
+print("outer loop")  
+print("inner loop")
+print("inner loop")
+print("inner loop")
+print("inner loop")
+print("inner loop")
