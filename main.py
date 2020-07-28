2468f4281c45cccbf032f1be85062f45f3323f7b
MyronFields1 <68288662+MyronFields@users.noreply.github.com>
2020-07-28 15:42:49 +0000

    Nim

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
-
+def my_function():
 # set player 1
-player = 1
+ player = 1
 
 # Game goal
 Goal = 20
+while True:
+ print("the number of objects is now ",goal)
+ move=input ("What is your move? ")
 
-move=input("What is your move? ")
-Goal = Goal = move
-print("your turn ",Goal)
+goal=goal=move
 
-if state==1:
-  
+print("the number of objects is now ",goal)
 
+if goal==1:
+  print("player ",player," wins!")
+'break'
 if player==1:
   player=2
 else:
   player=1
 
+     
+print("Game is over. ")
+
+
+
+
+#https://www.youtube.com/watch?v=NqsZ8DD6WHU
